23ee4aa98a98e1ad1d95b9da625e0df3cbf15866
Vishwanathms <vishwanath.murthy@gmail.com>
2023-03-31 16:26:59 +0530

    added git url

--- module.tf
+++ module.tf
@@ -20,7 +20,8 @@ provider "aws" {
 }
 
 module "vm01" {
-    source = "./ec2"
+    #source = "./ec2"
+    source =  git::"https://github.com/Vishwanathms/tf-templates-aws.git?ref=master"
     ami = var.ami
     instance_type = var.instance_type
     vm_name = var.vm_name
